3344a538108888131a8ad22e6610bc54cd2a2ccc
Justin Fulton <jusfulton@gmail.com>
2021-07-06 18:09:56 -0700

    Issue with embed render

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavComponent from "./shared/NavComponent";
 import HeaderComponent from "./shared/HeaderComponent";
+import SurveyComponent from "./survey/SurveyComponent";
 
 function App() {
 	return (
 		<div className='container'>
 			<NavComponent />
 			<HeaderComponent />
+			<SurveyComponent />
 		</div>
 	);
 }
--- src/components/survey/SurveyComponent.js
+++ src/components/survey/SurveyComponent.js
@@ -1,1 +1,14 @@
 import React from "react";
+import { Widget } from "@typeform/embed-react";
+
+const SurveyComponent = () => {
+	return (
+		<Widget
+			id='P5ktthZr'
+			style={{ width: "50%", height: "500px" }}
+			className='my-form'
+		/>
+	);
+};
+
+export default SurveyComponent;
